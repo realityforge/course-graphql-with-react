f2174e575eb932db14e688c535183efe154030f1
Peter Donald <peter@realityforge.org>
2017-06-12 15:17:01 +1000

    Remove debug

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -25,7 +25,6 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        console.log(`http://127.0.0.1:3000/users/${args.id}`);
         return axios.get(`http://127.0.0.1:3000/users/${args.id}`).
                      //Have to get data internal part of response because that is the data that returns data
                      then(response => response.data);
