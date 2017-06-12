c626657270d0957678992bc980a1038e5979c783
Peter Donald <peter@realityforge.org>
2017-06-12 15:45:50 +1000

    Add notion of organization and add bidrectional field resolvable from each direction

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,38 +1,79 @@
-const graphql = require('graphql');
-const axios = require('axios');
+const graphql = require( 'graphql' );
+const axios = require( 'axios' );
 
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema,
+  GraphQLList,
+  GraphQLSchema
 } = graphql;
 
-const UserType = new GraphQLObjectType({
+function queryBackend( path ) {
+  return axios.get( `http://localhost:3000/${path}` ).
+               //Have to get data internal part of response because that is the data that returns data
+               then( response => response.data );
+}
+
+const OrganizationType = new GraphQLObjectType( {
+  name: 'Organization',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList( UserType ),
+
+      //Users does not directly exist so we need to add resolve
+      resolve( parentValue, args ) {
+        return queryBackend( `organizations/${parentValue.id}/users` );
+      }
+    }
+  })
+} );
+
+const UserType = new GraphQLObjectType( {
   name: 'User',
   fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    organization: {
+      type: OrganizationType,
+      // resolve is required when the field does not have a direct alignment
+      // with data which it resolved to. Our underlying model has organizationId
+      // while our graphql schema has organization "reference"
+      resolve( parentValue, args ) {
+        //parentValue is the object that represents outer user object.
+        //So we can grab the organizationId from underlying model and translate it
+        // into organization
+        return queryBackend( `organizations/${parentValue.organizationId}` );
+      }
+    }
   })
-});
+} );
 
-const RootQuery = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType( {
   name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {
-        return axios.get(`http://127.0.0.1:3000/users/${args.id}`).
-                     //Have to get data internal part of response because that is the data that returns data
-                     then(response => response.data);
+      resolve( parentValue, args ) {
+        return queryBackend( `users/${args.id}` );
+      }
+    },
+    organization: {
+      type: OrganizationType,
+      args: { id: { type: GraphQLString } },
+      resolve( parentValue, args ) {
+        return queryBackend( `organizations/${args.id}` );
       }
     }
   }
-});
+} );
 
-module.exports = new GraphQLSchema({
+module.exports = new GraphQLSchema( {
   query: RootQuery
-});
+} );
