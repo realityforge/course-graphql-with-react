dbfad8a3e8c06ced5ed0966d909e87436aa3ef2a
Peter Donald <peter@realityforge.org>
2017-06-16 15:29:29 +1000

    Use optimistic update for like action

--- lyrical/client/components/LyricList.js
+++ lyrical/client/components/LyricList.js
@@ -21,7 +21,7 @@ class LyricList extends Component {
         <div className="vote-box">
           <i
             className="material-icons"
-            onClick={ _ => this.onLike( id ) }
+            onClick={ _ => this.onLike( id, likes ) }
           >
             thumb_up
           </i>
@@ -31,11 +31,27 @@ class LyricList extends Component {
     );
   }
 
-  onLike( lyricId ) {
+  onLike( lyricId, currentLikes ) {
+    // This is a representation of what we think the response will look
+    // like. Apollo will apply this to data store and re-render immediately.
+    // It will then re-render with correct data when actual response returns
+    // from the server.
+    // The format of this is easiest to determine by just looking at what is
+    // returned from the server. WIth enough understanding of graphql it is
+    // also possible to guess structure from the request structure.
+    const expectedResponse = {
+      __typename: 'Mutation',
+      likeLyric: {
+        __typename: 'LyricType',
+        id: lyricId,
+        likes: currentLikes + 1
+      }
+    };
     this.props.mutate( {
       variables: {
         lyricId: lyricId
-      }
+      },
+      optimisticResponse: expectedResponse
     } );
   }
 }
