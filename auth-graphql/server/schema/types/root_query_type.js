1bdd563247780f693a33507edfe49239f8f194e8
Peter Donald <peter@realityforge.org>
2017-06-17 17:21:42 +1000

    Add some dummy data so graphql will not complain

--- auth-graphql/server/schema/types/root_query_type.js
+++ auth-graphql/server/schema/types/root_query_type.js
@@ -1,8 +1,11 @@
-const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const graphql = require( 'graphql' );
+const { GraphQLString, GraphQLObjectType } = graphql;
 
-const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
-});
+const RootQueryType = new GraphQLObjectType( {
+  name: 'RootQueryType',
+  fields: {
+    placeholder: { type: GraphQLString }
+  }
+} );
 
 module.exports = RootQueryType;
